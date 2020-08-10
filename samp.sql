4dbc8c76fc3ec9e8ddf9c1423cc8a21f1f64a13d
unknown <olivermonger123@gmail.com>
2020-08-10 13:26:38 +0100

    uploading db for development

--- samp.sql
+++ samp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2020 at 01:57 AM
+-- Generation Time: Aug 10, 2020 at 02:24 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -73,7 +73,7 @@ INSERT INTO `accounts` (`ID`, `pName`, `pPassword`, `pEmail`, `pAdminLevel`, `pL
 (19, 'testdialogc', '$2y$12$QDDrYibDbTblZ1jfPy60butWDkKhIn7fzb6/6qog6cXvLCTYvMbwC', 'example@example.com', 0, 0, 0, 'America', 100, 0, 1, 167, 27, 0, 50, 0, 0, 0),
 (20, 'quiztest1', '$2y$12$cSDOOkf3ThXATj.0XhDDXOd1SS1fz0i/dZ4iuAQYph2EOT1gCtRMu', '@', 0, 0, 0, 'Los Santos', 100, 0, 1, 23, 28, 0, 1000, 0, 0, 0),
 (21, 'testaccounts_1', '$2y$12$KjjyaDTEQBe1LTTDUUvGaOTqZsuFTWhrHHEa6fv0KiASgtEyJscGG', 'gmail@gmail.com', 0, 0, 0, 'Los Santos', 100, 0, 1, 72, 28, 0, 1000, 0, 0, 0),
-(22, 'testaccounts_2', '$2y$12$XFDYQjXvbjThLR/xKkjRTOarF5sW7jrTtLDUAQoj5KzHKM/NZ3rRa', 'olly@yllo.co.uk', 6, 2, 3, 'Los Santos', 75, 0, 1, 73, 28, 6695, 1000, 35, 1, 351),
+(22, 'testaccounts_2', '$2y$12$XFDYQjXvbjThLR/xKkjRTOarF5sW7jrTtLDUAQoj5KzHKM/NZ3rRa', 'olly@yllo.co.uk', 6, 2, 4, 'Los Santos', 71, 0, 1, 73, 28, 7220, 1000, 55, 1, 528),
 (23, 'testaccounts_3', '$2y$12$PEeuZSHTTVnjKDb2UEDWSunyPVyEv7lKRwTEZihlRmQmObQ2NP8n.', '@', 0, 1, 1, 'Los Santos', 100, 0, 1, 170, 28, 0, 1000, 60, 1, 0),
 (24, 'testaccounts_6', '$2y$12$YzDJSCnlPVH/ShTMXyzwOOEfQvuVt/Ty88apHkAAldDO5rmIFSJvi', '@', 0, 1, 1, 'america', 100, 0, 1, 24, 28, 0, 1000, 59, 1, 60),
 (25, 'testaccount_12', '$2y$12$MUbIMBbtUDjFaTHrSS3sbeR/.2zeCYsrJAsxX95yyYweQUtXfsQRK', '@', 0, 1, 1, 'America', 100, 0, 1, 60, 28, 0, 1000, 59, 1, 50);
@@ -101,6 +101,37 @@ INSERT INTO `jobs` (`jID`, `jName`, `jPay`, `jobIX`, `jobIY`, `jobIZ`) VALUES
 (1, 'Postman', 8, -88.8793, 1124.12, 19.3012),
 (2, 'Garbageman', 12, 285.13, 1406.48, 10.5003);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `vID` int(11) NOT NULL,
+  `vModelId` int(11) NOT NULL,
+  `vOwner` varchar(32) NOT NULL,
+  `vFuel` int(11) NOT NULL,
+  `vJobId` int(11) NOT NULL,
+  `vFacId` int(11) NOT NULL,
+  `vPlate` varchar(32) NOT NULL,
+  `vParkedX` float NOT NULL,
+  `vParkedY` float NOT NULL,
+  `vParkedZ` float NOT NULL,
+  `vAngle` float NOT NULL,
+  `vRentalState` int(11) NOT NULL,
+  `vRentalPrice` int(11) NOT NULL,
+  `vColor1` int(11) NOT NULL,
+  `vColor2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vehicles`
+--
+
+INSERT INTO `vehicles` (`vID`, `vModelId`, `vOwner`, `vFuel`, `vJobId`, `vFacId`, `vPlate`, `vParkedX`, `vParkedY`, `vParkedZ`, `vAngle`, `vRentalState`, `vRentalPrice`, `vColor1`, `vColor2`) VALUES
+(1, 411, 'NULL', 100, 0, 0, 'TESTING1', -204.533, 1119.16, 23.2031, 269.15, 1, 500, 1, 1);
+
 --
 -- Indexes for dumped tables
 --
@@ -118,6 +149,12 @@ ALTER TABLE `jobs`
   ADD PRIMARY KEY (`jID`);
 
 --
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`vID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -132,6 +169,12 @@ ALTER TABLE `accounts`
 --
 ALTER TABLE `jobs`
   MODIFY `jID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `vID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
